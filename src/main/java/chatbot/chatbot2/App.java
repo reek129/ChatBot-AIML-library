5975b487b7b6d68987ee73119a9bf4e4e4a75819
rick129 <reek123@gmail.com>
2018-06-08 12:21:00 +0530

    First AIML ChatBOT

---
+++ src/main/java/chatbot/chatbot2/App.java
@@ -0,0 +1,13 @@
+package chatbot.chatbot2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
